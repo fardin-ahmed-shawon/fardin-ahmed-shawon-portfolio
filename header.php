4294fda2a2a4430d1499f5458fcef4ce1523453f
fardin-ahmed-shawon <mdshawon7443@gmail.com>
2025-07-27 00:26:38 +0600

    up to date

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 	<!-- Required meta tags -->
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="icon" href="img/favicon.png" type="image/png">
+	<link rel="icon" href="img/fav.png" type="image/png">
 	<title><?= $page_title ?></title>
 	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="css/bootstrap.css">
